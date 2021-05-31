17ceaaa5505c0c3a45faadad8e17c1994f74fc04
Andrew Brannan <andrew.brannan@gmail.com>
2021-05-31 20:10:27 +0100

    prettier

--- app.js
+++ app.js
@@ -5,51 +5,51 @@ const Xray = require("x-ray");
 const sgMail = require("@sendgrid/mail");
 
 async function sendMail(stockText, url) {
-  sgMail.setApiKey(config.sendGridKey);
-  const msg = {
-    to: config.email,
-    from: config.email,
-    subject: `S5 Stock Alert!}`,
-    text: `url: ${url}\nStock status: ${stockText}`,
-  };
-  await sgMail.send(msg);
-  console.log("Email sent");
+    sgMail.setApiKey(config.sendGridKey);
+    const msg = {
+        to: config.email,
+        from: config.email,
+        subject: `S5 Stock Alert!}`,
+        text: `url: ${url}\nStock status: ${stockText}`,
+    };
+    await sgMail.send(msg);
+    console.log("Email sent");
 }
 
 async function runStockChecker(message, context) {
-  // Unpack pubsub message
-  const urlEncoded = message.attributes.url;
+    // Unpack pubsub message
+    const urlEncoded = message.attributes.url;
 
-  if (!urlEncoded) {
-    throw new Error("No url attribute found in pubsub message, exiting!");
-  }
-
-  // Decode URL
-  const url = Buffer.from(urlEncoded, "base64").toString();
-
-  console.log(`Running stock checker!`);
-  console.log(`   url:        ${url}`);
-  console.log(`   send email: ${config.sendEmail}`);
-  console.log(`   recipient:  ${config.recipientEmail}`);
-  console.log(`   time:       ${context.timestamp}`);
-
-  // Check the status
-  const xray = Xray();
-  const status = await xray(url, ".stock");
-  console.log(`Stock status: ${status}`);
-
-  // Send an email if requested
-  if (status.toLowerCase().includes("in stock")) {
-    console.log("Stock found!");
-    if (config.sendEmail) {
-      console.log("Sending email!");
-      await sendMail(status, url);
+    if (!urlEncoded) {
+        throw new Error("No url attribute found in pubsub message, exiting!");
     }
-  } else if (status.toLowerCase().includes("out of stock")) {
-    console.log("Out of stock.");
-  } else {
-    console.log("Unknown stock status.");
-  }
+
+    // Decode URL
+    const url = Buffer.from(urlEncoded, "base64").toString();
+
+    console.log(`Running stock checker!`);
+    console.log(`   url:        ${url}`);
+    console.log(`   send email: ${config.sendEmail}`);
+    console.log(`   recipient:  ${config.recipientEmail}`);
+    console.log(`   time:       ${context.timestamp}`);
+
+    // Check the status
+    const xray = Xray();
+    const status = await xray(url, ".stock");
+    console.log(`Stock status: ${status}`);
+
+    // Send an email if requested
+    if (status.toLowerCase().includes("in stock")) {
+        console.log("Stock found!");
+        if (config.sendEmail) {
+            console.log("Sending email!");
+            await sendMail(status, url);
+        }
+    } else if (status.toLowerCase().includes("out of stock")) {
+        console.log("Out of stock.");
+    } else {
+        console.log("Unknown stock status.");
+    }
 }
 
 exports.runStockChecker = runStockChecker;
--- app.test.js
+++ app.test.js
@@ -10,39 +10,41 @@ const Xray = require("x-ray");
 const sgMail = require("@sendgrid/mail");
 
 beforeEach(() => {
-  // https://test.com base64 === "aHR0cHM6Ly90ZXN0LmNvbQ=="
-  fixtures.pubsub_message = { attributes: { url: "aHR0cHM6Ly90ZXN0LmNvbQ==" } };
-  fixtures.context = { timestamp: "2018-04-09T07:56:12.975Z" };
+    // https://test.com base64 === "aHR0cHM6Ly90ZXN0LmNvbQ=="
+    fixtures.pubsub_message = {
+        attributes: { url: "aHR0cHM6Ly90ZXN0LmNvbQ==" },
+    };
+    fixtures.context = { timestamp: "2018-04-09T07:56:12.975Z" };
 
-  config.sendEmail = true;
-  config.sendGridKey = "DUMMYKEY";
+    config.sendEmail = true;
+    config.sendGridKey = "DUMMYKEY";
 });
 
 test("runner to error if no url in message", async () => {
-  fixtures.pubsub_message = { attributes: { notAUrl: 2 } };
-  await expect(async () => {
-    await runStockChecker(fixtures.pubsub_message, fixtures.context);
-  }).rejects.toThrowError();
+    fixtures.pubsub_message = { attributes: { notAUrl: 2 } };
+    await expect(async () => {
+        await runStockChecker(fixtures.pubsub_message, fixtures.context);
+    }).rejects.toThrowError();
 });
 
 test("runner detects oos", async () => {
-  Xray.mockReturnValue(async (url, id) => {
-    expect(url).toBe("https://test.com");
-    expect(id).toBe(".stock");
-    return "Out of stock";
-  });
-  await runStockChecker(fixtures.pubsub_message, fixtures.context);
-  expect(sgMail.send).toHaveBeenCalledTimes(0);
+    Xray.mockReturnValue(async (url, id) => {
+        expect(url).toBe("https://test.com");
+        expect(id).toBe(".stock");
+        return "Out of stock";
+    });
+    await runStockChecker(fixtures.pubsub_message, fixtures.context);
+    expect(sgMail.send).toHaveBeenCalledTimes(0);
 });
 
 test("runner sends email if not OOS", async () => {
-  Xray.mockReturnValue(async (url, id) => {
-    expect(url).toBe("https://test.com");
-    expect(id).toBe(".stock");
-    return "5 in stock";
-  });
-  await runStockChecker(fixtures.pubsub_message, fixtures.context);
-  expect(sgMail.send).toHaveBeenCalledTimes(1);
-  expect(sgMail.setApiKey).toHaveBeenCalledWith("DUMMYKEY");
-  expect(sgMail.send).toHaveBeenCalledTimes(1);
+    Xray.mockReturnValue(async (url, id) => {
+        expect(url).toBe("https://test.com");
+        expect(id).toBe(".stock");
+        return "5 in stock";
+    });
+    await runStockChecker(fixtures.pubsub_message, fixtures.context);
+    expect(sgMail.send).toHaveBeenCalledTimes(1);
+    expect(sgMail.setApiKey).toHaveBeenCalledWith("DUMMYKEY");
+    expect(sgMail.send).toHaveBeenCalledTimes(1);
 });
